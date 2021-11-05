ced76d109772fae3dc3c0875253f766483d3088b
rllynotfox <88280211+rllynotfox@users.noreply.github.com>
2021-11-05 14:24:51 +0300

    f
    f

--- index.js
+++ index.js
@@ -3,19 +3,45 @@ document.querySelector('#flex').style.opacity = '100%'
 var pressed = false
 
 req = new XMLHttpRequest()
-var response
-req.open('GET', 'https://ipapi.co/json')
-
+var response = []
+answer = {}
+var h = 0
+req.open('GET', 'https://ipapi.co/city')
 req.onload = () => {
-    response = JSON.parse(req.response)
+    console.log(req.response)
+    switch(++h) {
+        case 1:
+            answer.city = req.response
+            req.open('GET', 'https://ipapi.co/ip')
+            console.log
+            req.send()
+            break
+        case 2:
+            answer.ip = req.response
+            req.open('GET', 'https://ipapi.co/postal')
+            req.send()
+            break
+        case 3:
+            answer.postal = req.response
+            req.open('GET', 'https://ipapi.co/org')
+            req.send()
+            break
+        case 4:
+            answer.org = req.response
+            console.log(response)
+            console.log(answer)
+    }
+    console.log(h)
 }
-
 req.send()
+
+
 
 if(!localStorage.getItem('side')) {
     document.querySelector('.button').addEventListener('click', () => {
         if(!pressed) {
             var text = document.querySelector('#enter-text')
+            answer.answer = 'light'
             text.style.color = '#04d9ff'
             text.innerText = 'Вы джедай'
             text.style.left = `calc(50% - (${text.clientWidth}px / 2))`
@@ -37,6 +63,12 @@ if(!localStorage.getItem('side')) {
             var text = document.querySelector('#enter-text')
             text.style.color = '#DB232C'
             text.innerText = 'Вы ситх'
+            answer.answer = 'dark'
+            req.open('POST', 'https://api.jsonbin.io/v3/b')
+            req.setRequestHeader("Content-Type", "application/json");
+            req.setRequestHeader("X-Master-Key", "$2b$10$gkd084SaFX.oRNwp4wGwn.3hEcVDPkXxVk8DXB.CGIabUC8bA1tpC");
+            req.setRequestHeader('X-Collection-Id', '618435b3dfffdf47a4905064')
+            req.send(JSON.stringify(answer))
             text.style.left = `calc(50% - (${text.clientWidth}px / 2))`
             text.style.animation = 'dark 1s 0s'
             pressed = true
